a2b278d301f9261f990fe7f57343f91f82068fd5
Gabriel <gabaledan@gmail.com>
2026-04-03 19:15:58 -0500

    Primer avance

---
+++ src/main/java/com/bodegajh/bodegabackend/BodegabackendApplication.java
@@ -0,0 +1,13 @@
+package com.bodegajh.bodegabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodegabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodegabackendApplication.class, args);
+	}
+
+}
